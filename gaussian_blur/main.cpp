47dd55ce96fc523344a2bb0292c1a95d06e1f77e
Константин Сандалов <sandaloff.constantin@yandex.ru>
2023-05-30 12:10:28 +0300

    Updated main in gaussian_blur

--- gaussian_blur/main.cpp
+++ gaussian_blur/main.cpp
@@ -1,7 +1,7 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <iostream>
-// #include <opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
 
 #include <ctime>
 
@@ -73,3 +73,121 @@ float** create2DKernel(int size, float sigma)
     }
 
     return kernel;
+}
+
+void clear2DKernel(float*** kernel, int size)
+{
+    for (int i = 0; i < size; ++i) {
+        delete((*kernel)[i]);
+    }
+    delete[](*kernel);
+    *kernel = nullptr;
+}
+void clear1DKernel(float** kernel, int size)
+{
+    delete(*kernel);
+    *kernel = nullptr;
+}
+
+void printKernel(float** kernel, int size)
+{
+    for (int x = 0; x < size; ++x) {
+        for (int y = 0; y < size; ++y) {
+            std::cout << kernel[x][y] << ' ';
+        }
+        std::cout << std::endl;
+    }
+}
+void print1DKernel(float* kernel, int size)
+{
+    for (int x = 0; x < size; ++x) {
+        std::cout << kernel[x] << ' ';
+    }
+    std::cout << std::endl;
+}
+
+void horisontalBlur(float* kernel, float** src_image, float** res_image, int img_size, int kernel_size)
+{
+    int half_size = std::floor(kernel_size / 2.);
+    for (int i = 0; i < img_size; ++i)
+        for (int j = half_size; j < img_size - half_size; ++j) {
+            for (int k_j = -half_size; k_j <= half_size; ++k_j) {
+                res_image[i][j] += src_image[i][j + k_j] * kernel[k_j + half_size];
+            }
+        }
+}
+
+void verticalBlur(float* kernel, float** src_image, float** res_image, int img_size, int kernel_size)
+{
+    int half_size = std::floor(kernel_size / 2.);
+    for (int j = half_size; j < img_size - half_size; ++j) {
+        for (int k_j = -half_size; k_j <= half_size; ++k_j) {
+            for (int i = 0; i < img_size; ++i)
+                res_image[j][i] += src_image[j + k_j][i] * kernel[k_j + half_size];
+        }
+    }
+}
+
+void blur(float** kernel, float** src_image, float** res_image, int img_size, int kernel_size)
+{
+    int half_size = std::floor(kernel_size / 2.);
+    for (int i = half_size; i < img_size - half_size; ++i)
+        for (int j = half_size; j < img_size - half_size; ++j) {
+            for (int k_i = -half_size; k_i <= half_size; ++k_i)
+#pragma simd reduction(+:res_image[i][j])
+                for (int k_j = -half_size; k_j <= half_size; ++k_j) {
+                    res_image[i][j] += src_image[i + k_i][j + k_j] *
+                        kernel[k_i + half_size][k_j + half_size];
+                }
+        }
+}
+
+int main(int argc, char** argv)
+{
+    clock_t start, stop;
+    int k_size = 15, sigma = 1, i_size = 2000;
+    float** src_img = createImage(i_size);
+    //src_img[3][3] = 128;
+    float** res_img = createImage(i_size);
+    //resetImage(res_img, i_size);
+    float** kernel = create2DKernel(k_size, sigma);
+    std::cout << "Gaussian kernel:\n";
+    printKernel(kernel, k_size);
+    start = clock();
+    blur(kernel, src_img, res_img, i_size, k_size);
+    stop = clock();
+    std::cout << "Elapsed time simple = " << ((double)(stop - start)) / CLOCKS_PER_SEC << " seconds" << std::endl;
+
+    //printKernel(res_img, i_size);
+    //resetImage(res_img, i_size);
+
+    //float* krnl1D = create1DKernel(k_size, sigma);
+    //print1DKernel(krnl1D, k_size);
+    //start = clock();
+    ////printKernel(src_img, i_size);
+    //horisontalBlur(krnl1D, src_img, res_img, i_size, k_size);
+    //resetImage(src_img, i_size);
+    //verticalBlur(krnl1D, res_img, src_img, i_size, k_size);
+    //stop = clock();
+    //std::cout << "Elapsed time simple = " << ((double)(stop - start)) / CLOCKS_PER_SEC << " seconds" << std::endl;
+
+    //printKernel(src_img, i_size);
+
+    clear2DKernel(&kernel, k_size);
+    clear2DKernel(&src_img, i_size);
+    clear2DKernel(&res_img, i_size);
+    //clear1DKernel(&krnl1D, k_size);
+    /*cv::Mat image = cv::imread("C:\\Users\\k7912\\source\\repos\\GaussianBlur\\x64\\Release\\enna.png", cv::IMREAD_GRAYSCALE);
+    if (image.empty()) {
+        std::cout << "Image not found\n";
+    }*/
+
+    //try {
+    //    //cv::imshow("name", image);
+    //}
+    //catch (cv::Exception ex) {
+    //    std::cout << ex.what() << std::endl;
+    //}
+
+    return 0;
+}
